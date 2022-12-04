d8714b3f0317c061478920c5c2ce047fb97d75b7
Markus Witt <feuerrot@feuerrot.org>
2022-12-04 23:19:55 +0100

    refactor: compact list of tasks

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -8,54 +9,25 @@ import (
 type Task func(string) (int, error)
 
 func main() {
-	days := []struct {
-		day   int
-		input string
-		tasks []Task
-	}{
-		{
-			day:   1,
-			input: "input/202201",
-			tasks: []Task{
-				AOC2022011,
-				AOC2022012,
-			},
-		}, {
-			day:   2,
-			input: "input/202202",
-			tasks: []Task{
-				AOC2022021,
-				AOC2022022,
-			},
-		}, {
-			day:   3,
-			input: "input/202203",
-			tasks: []Task{
-				AOC2022031,
-				AOC2022032,
-			},
-		}, {
-			day:   4,
-			input: "input/202204",
-			tasks: []Task{
-				AOC2022041,
-				AOC2022042,
-			},
-		},
+	tasks := map[int][]Task{
+		1: {AOC2022011, AOC2022012},
+		2: {AOC2022021, AOC2022022},
+		3: {AOC2022031, AOC2022032},
+		4: {AOC2022041, AOC2022042},
 	}
 
-	for _, day := range days {
-		input, err := os.ReadFile(day.input)
+	for day := range tasks {
+		input, err := os.ReadFile(fmt.Sprintf("input/2022%02d", day))
 		if err != nil {
 			log.Fatalf("can't open input: %v", err)
 		}
 
-		for i, task := range day.tasks {
+		for i, task := range tasks[day] {
 			res, err := task(string(input))
 			if err != nil {
-				log.Fatalf("error in Task %d/%d: %v", day.day, i+1, err)
+				log.Fatalf("error in Task %d/%d: %v", day, i+1, err)
 			}
-			log.Printf("AOC2022 %02d/%d: %d", day.day, i+1, res)
+			log.Printf("AOC2022 %02d/%d: %d", day, i+1, res)
 		}
 	}
 }
